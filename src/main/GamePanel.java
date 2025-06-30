56f4ede91b4dbfdf4cccf56f9666526dc4bfcad1
CezarGarcia <cezargarcia2003@gmail.com>
2025-06-29 23:30:46 -0300

    Added objects

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,6 +1,7 @@
 package main;
 
 import entity.Player;
+import object.SuperObject;
 import tile.TileManager;
 
 import javax.swing.*;
@@ -33,7 +34,9 @@ public class GamePanel extends JPanel implements Runnable {
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     public CollisionChecker cChecker = new CollisionChecker(this);
+    public AssetSetter aSetter = new AssetSetter(this);
     public Player player = new Player(this, keyH);
+    public SuperObject obj[] = new SuperObject[10];
 
     private boolean running = false;
 
@@ -46,6 +49,10 @@ public class GamePanel extends JPanel implements Runnable {
         setupKeyBindings();
         // Request focus to ensure key events are captured
         this.requestFocusInWindow();
+    }
+
+    public void setupGame() {
+        aSetter.setObject();
     }
 
     private void setupKeyBindings() {
@@ -177,8 +184,17 @@ public class GamePanel extends JPanel implements Runnable {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
 
+        //Tile
         tileM.draw(g2);
 
+        //Object
+        for (int i = 0; i < obj.length; i++) {
+            if (obj[i] != null) {
+                obj[i].draw(g2, this);
+            }
+        }
+
+        //Player
         player.draw(g2);
 
         g2.dispose();
